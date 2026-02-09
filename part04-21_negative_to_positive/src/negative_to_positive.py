cecb701df2ed7c7b2740d53b8cc4caa9824ddd56
Asif Ali <asephkhan@gmail.com>
2026-02-09 22:39:29 +0000

    till part 4 palindroms excercise is done

--- part04-21_negative_to_positive/src/negative_to_positive.py
+++ part04-21_negative_to_positive/src/negative_to_positive.py
@@ -1,1 +1,9 @@
 # Write your solution here
+N = int(input("Please type in a positive integer:"))
+
+for i in range(-N, N+1):
+#    print(i)
+#for i in range(1, N+1):
+#    print(i)
+    if i != 0:
+        print(i)
--- part04-22_list_of_stars/src/list_of_stars.py
+++ part04-22_list_of_stars/src/list_of_stars.py
@@ -1,1 +1,11 @@
 # Write your solution here
+def list_of_stars(my_list: list):
+    for i in my_list:
+        print(i * "*")
+
+#list_of_stars([3, 7, 1, 1, 2])
+
+
+if __name__ == "__main__":
+    list_of_stars([3, 7, 1, 1, 2])
+    
