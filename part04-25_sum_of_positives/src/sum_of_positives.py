2716818aea53aad585376670782da9ab955dfd8e
Asif Ali <asephkhan@gmail.com>
2026-02-10 21:40:05 +0000

    finished difinitive iterations part of part 4

--- part04-25_sum_of_positives/src/sum_of_positives.py
+++ part04-25_sum_of_positives/src/sum_of_positives.py
@@ -1,1 +1,12 @@
 # Write your solution here
+def sum_of_positives(my_list: list):
+    sum = 0
+    for i in my_list:
+        if (i > 0):
+            sum = i + sum
+    return sum
+
+if __name__ == "__main__":
+    my_list = [1, -2, 3, -4, 5]
+    result = sum_of_positives(my_list)
+    print("The result is", result)
