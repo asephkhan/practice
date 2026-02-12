cb8fdbe89896afab44b1fcc14491e7692e494a14
Asif Ali <asephkhan@gmail.com>
2026-02-12 23:40:52 +0000

    started grade statistics

--- part04-33_everything_reversed/src/everything_reversed.py
+++ part04-33_everything_reversed/src/everything_reversed.py
@@ -1,1 +1,11 @@
 # Write your solution here
+def everything_reversed(my_list: list):
+    reversed_list = my_list[::-1]
+    result = []
+    for i in reversed_list:
+        result.append(i[::-1])
+    return result
+if __name__ == "__main__":
+    my_list = ["Hi", "there", "example", "one more"]
+    new_list = everything_reversed(my_list)
+    print(new_list)
--- part04-38_grade_statistics/src/grade_statistics.py
+++ part04-38_grade_statistics/src/grade_statistics.py
@@ -1,1 +1,29 @@
 # Write your solution here
+#def main():
+#    while True:
+#        user_entry = input("Exam points and excercises completed: ")
+#        if user_entry == "":
+#            print("Stats")            
+#            break            
+#main()
+excercises = [87,60]
+exam_points = [18, 14]
+
+def completed_excercises_conversion(courses: list):
+    excercises_points = []
+    for excercises in courses:
+        excercises_points.append(excercises//10)
+    print("excercise points: ", excercises_points)
+    return excercises_points
+
+excercises_points = completed_excercises_conversion(excercises)
+
+# function to calculate all points
+def calculate_total_points(excercises_points: list, exam_points:list):
+    total_points = []
+    for points in range(len(excercises_points)):
+        total_points.append(excercises_points[points] + exam_points[points])
+    print("total points: ", total_points)    
+    return total_points
+
+total_points = calculate_total_points(excercises_points,exam_points)
