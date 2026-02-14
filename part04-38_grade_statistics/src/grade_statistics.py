06be5b8859d0a24952ccbf3a4ebb2a78564ce924
Asif Ali <asephkhan@gmail.com>
2026-02-14 23:50:25 +0000

    still going with grade statistics

--- part04-38_grade_statistics/src/grade_statistics.py
+++ part04-38_grade_statistics/src/grade_statistics.py
@@ -1,29 +1,75 @@
 # Write your solution here
-#def main():
-#    while True:
-#        user_entry = input("Exam points and excercises completed: ")
-#        if user_entry == "":
-#            print("Stats")            
-#            break            
-#main()
-excercises = [87,60]
-exam_points = [18, 14]
-
-def completed_excercises_conversion(courses: list):
+# function to calculate excercises points
+def completed_excercises_conversion(users: list):
     excercises_points = []
-    for excercises in courses:
+    for excercises in users:
         excercises_points.append(excercises//10)
-    print("excercise points: ", excercises_points)
     return excercises_points
-
-excercises_points = completed_excercises_conversion(excercises)
-
+    
 # function to calculate all points
 def calculate_total_points(excercises_points: list, exam_points:list):
-    total_points = []
-    for points in range(len(excercises_points)):
-        total_points.append(excercises_points[points] + exam_points[points])
-    print("total points: ", total_points)    
-    return total_points
+    total_points = 0
 
-total_points = calculate_total_points(excercises_points,exam_points)
+    for points in range(len(excercises_points)):
+        total_points += (excercises_points[points] + exam_points[points])    
+    return total_points
+    
+# function to calculate average
+def calculate_average_points(total_points, number_of_users):
+    average = float(total_points/number_of_users)
+    return average
+# function to calculate percentage of passing students
+def calculate_percentage_of_passing_students(passed_students: int, total_students: int):
+    percentage = (passed_students* 100 )/total_students
+    return percentage
+
+def calculate_grades(excercises_points: list, exam_points:list):
+    grades = []
+
+    for points in range(len(excercises_points)):
+        total_points = (excercises_points[points] + exam_points[points]) 
+        if exam_points[points] < 10:
+            grades.append(0)
+        elif total_points >= 28:
+            grades.append(5)
+        elif total_points >= 24:
+            grades.append(4)
+        elif total_points >= 21:
+            grades.append(3)
+        elif total_points >= 18:
+            grades.append(2)
+        elif total_points >= 14:
+            grades.append(1)
+        else:
+            grades.append(0)
+    
+    return grades
+
+
+# function to print stats
+def print_stats(average, percentage, grades):
+    print("Average points:", average)
+    print("Percentage:", percentage)
+    print("Grade Statistcs:", grades)
+    return
+
+def main():
+    excercises = []
+    exam_points = []
+    passed_students = 0
+    while True:
+        user_entry = input("Exam points and excercises completed: ")
+        if user_entry == "":
+            excercises_points = completed_excercises_conversion(excercises) 
+            total_points = calculate_total_points(excercises_points,exam_points)
+            average = calculate_average_points(total_points, len(exam_points))
+            percentage = calculate_percentage_of_passing_students(passed_students,len(exam_points))
+            grades = calculate_grades(excercises_points,exam_points)            
+            print_stats(average, percentage, grades)            
+            break
+        parts = user_entry.split()
+        exam_points.append(int(parts[0]))   
+        excercises.append(int(parts[1])) 
+        if int(parts[0]) >= 10 and int(parts[0]) + (int(parts[1])//10) >= 15 :
+            passed_students += 1
+main()
