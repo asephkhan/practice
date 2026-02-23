bbb3c6a64c7ed283a8b06d0995ede4b7086c53af
Asif Ali <asephkhan@gmail.com>
2026-02-23 20:13:40 +0000

    part 5 references

--- part05-06_sudoku_block/src/sudoku_block.py
+++ part05-06_sudoku_block/src/sudoku_block.py
@@ -1,6 +1,16 @@
 # Write your solution here
 def block_correct(sudoku: list, row_no: int, column_no: int):
     numbers = []
-    for row in range(sudoku(row_no: 3: 1)):
-        for square in row range(sudoku(row_no: 3: 1))
+    
+    for r in range(row_no, row_no + 3):
+        for c in range(column_no, column_no + 3):
+            square = sudoku[r][c]
+            if square > 0 and square in numbers:
+                return False
+            else:
+                numbers.append(square)
+    return True
+   
+    print(sudoku[row_no][column_no])
+
         
--- part05-09_remove_smallest/src/remove_smallest.py
+++ part05-09_remove_smallest/src/remove_smallest.py
@@ -1,1 +1,8 @@
 # Write your solution here
+def remove_smallest(numbers: list):
+    smallest = numbers[0]
+    for number in numbers:
+        if number <= smallest:
+            smallest = number
+    numbers.remove(smallest)
+
