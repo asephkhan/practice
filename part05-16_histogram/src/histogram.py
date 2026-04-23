3a747308e9a19c884c30c2f10cfecc25be761777
Asif Ali <asephkhan@gmail.com>
2026-04-23 17:18:04 +0100

    started dictionaries

--- part05-16_histogram/src/histogram.py
+++ part05-16_histogram/src/histogram.py
@@ -1,1 +1,13 @@
 # Write your solution here
+
+def histogram(string : str):
+    dictionary = {}    
+    for letter in string:
+        if letter in dictionary:
+            dictionary[letter] += "*"       
+        if letter not in dictionary:
+            dictionary[letter] = "*"
+    for key, value in dictionary.items():
+        print(key, value)
+if __name__ == "__main__":
+    histogram("hello!")
