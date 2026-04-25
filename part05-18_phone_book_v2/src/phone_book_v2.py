50bc3931e5b7ec43d7f391981fe746501b612322
Asif Ali <asephkhan@gmail.com>
2026-04-25 23:43:19 +0100

    phonebook v2

--- part05-18_phone_book_v2/src/phone_book_v2.py
+++ part05-18_phone_book_v2/src/phone_book_v2.py
@@ -1,1 +1,28 @@
 # Write your solution here
+def add(persons):
+    name = input("name:")
+    number = input("number: ")
+    persons[name] = number
+    print("ok!")
+
+def search(persons):
+    name = input("name:")
+    for key in persons:
+        if name in persons:        
+            print(persons[key])
+        else:
+            print("no number")
+
+def main():
+    persons = {}
+    while True:
+        cmd = input("command (1 search, 2 add, 3 quit): ")
+        if cmd == "1":
+            search(persons)
+        if cmd == "2":
+            add(persons)
+        if cmd == "3":
+            break
+    print("quitting...")
+
+main()
