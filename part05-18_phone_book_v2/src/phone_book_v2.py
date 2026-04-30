220815102ce3a192ba88e49e89c029d30fb7ebfa
Asif Ali <asephkhan@gmail.com>
2026-04-30 23:50:55 +0100

    phonebook v2

--- part05-18_phone_book_v2/src/phone_book_v2.py
+++ part05-18_phone_book_v2/src/phone_book_v2.py
@@ -2,16 +2,20 @@
 def add(persons):
     name = input("name:")
     number = input("number: ")
-    persons[name] = number
+    if name not in persons:
+        persons[name] = []
+    persons[name].append(number)
     print("ok!")
 
 def search(persons):
     name = input("name:")
-    for key in persons:
-        if name in persons:        
-            print(persons[key])
-        else:
-            print("no number")
+    if name in persons: 
+        numbers = persons[name]
+        for number in numbers:       
+            print(number)
+    else:
+        print("no number")
+
 #main
 def main():
     persons = {}
